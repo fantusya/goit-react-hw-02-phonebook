fc3b6836efd6885cba66145ad5ca9091c7c2c837
Anastasiia <guba.anastaysha@gmail.com>
2022-10-28 01:40:59 +0300

    renamed

--- src/components/App/App.styles.jsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const PageTitle = styled.h1`
-  color: ${p => p.theme.colors.accent};
-  font-family: ${p => p.theme.fonts.heading};
-  font-size: ${p => p.theme.fontSizes.xl};
-  font-weight: ${p => p.theme.fontWeights.normal};
-  margin-bottom: ${p => p.theme.space[5]}px;
-`;
