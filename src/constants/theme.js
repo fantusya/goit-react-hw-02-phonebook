9efc4592608695070b7274ad6ad401000debb2c0
Anastasiia <guba.anastaysha@gmail.com>
2022-10-27 22:20:46 +0300

    Create theme.js

---
+++ src/constants/theme.js
@@ -0,0 +1,46 @@
+export const theme = {
+  colors: {
+    black: '#000',
+    white: '#fff',
+    red: '#B22222',
+    golden: '#FFD700',
+    blue: '#00008B',
+    silver: '#C0C0C0',
+    text: '#4682B4',
+    accent: '#F0F8FF',
+    secondary: '#0000FF',
+    border: '#add8e6',
+  },
+  space: [0, 2, 4, 8, 16, 32, 64, 128, 256],
+  fonts: {
+    body: 'Arial, sans-serif',
+    heading: 'Impact',
+    monospace: 'Menlo, monospace',
+  },
+  fontSizes: {
+    xs: '12px',
+    s: '14px',
+    m: '16px',
+    ml: '20px',
+    lm: '25px',
+    l: '32px',
+    xl: '64px',
+  },
+  fontWeights: {
+    normal: 400,
+    bold: 700,
+  },
+  lineHeights: {
+    body: 1.5,
+    heading: 1.125,
+  },
+  borders: {
+    none: 'none',
+    normal: '1px solid',
+  },
+  radii: {
+    none: '0',
+    normal: '15px',
+    round: '50%',
+  },
+};
